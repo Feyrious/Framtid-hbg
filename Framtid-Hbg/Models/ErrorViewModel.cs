c5d8d543deb5d74c41ef478e01fdcaf71cfd7bed
Kim Rydén <threezool@gmail.com>
2025-08-12 13:09:26 +0200

    Added Asp.Net MVC Project

---
+++ Framtid-Hbg/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Framtid_hbg.Website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
