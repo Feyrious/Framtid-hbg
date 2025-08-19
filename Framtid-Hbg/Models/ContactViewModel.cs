9b87bcc7a50c1d8b925781e275d697d2cdc6ac8e
Kim Rydén <threezool@gmail.com>
2025-08-19 16:50:06 +0200

    Added a contact model mapping to the contact form

---
+++ Framtid-Hbg/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Framtid_hbg.Website.Models;
+
+public class ContactViewModel
+{
+    [BindProperty]
+    public string? ContactType { get; set; }
+    
+    [BindProperty]
+    public string? Name { get; set; }
+    
+    [BindProperty]
+    public string? Email { get; set; }
+    
+    [BindProperty]
+    public int PhoneNumber { get; set; }
+    
+    [BindProperty]
+    public string? Adress { get; set; }
+    
+    [BindProperty]
+    public string? Message { get; set; }
+}
