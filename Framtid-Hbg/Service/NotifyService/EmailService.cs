eba816027d3e4480decf4adaa70a6d0afefb6d5e
Kim Rydén <threezool@gmail.com>
2025-08-21 13:17:24 +0200

    Updated smtp client settings for secure delivery

--- Framtid-Hbg/Service/NotifyService/EmailService.cs
+++ Framtid-Hbg/Service/NotifyService/EmailService.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
 using Framtid_hbg.Website.Service.Interface;
 
@@ -6,16 +6,27 @@ namespace Framtid_hbg.Website.Service;
 
 public class EmailService : INotifyService
 {
+    private readonly string _host;
+    private readonly int _port;
+    private readonly string _username;
+    private readonly string _password;
+    private readonly string _to;
+    
+    public EmailService()
+    {
+        _host = Environment.GetEnvironmentVariable("SMTP_HOST") ?? string.Empty;
+        _port = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT") ?? "0");
+        _username = Environment.GetEnvironmentVariable("SMTP_USERNAME") ?? string.Empty;
+        _password = Environment.GetEnvironmentVariable("SMTP_PASSWORD") ?? string.Empty;
+        _to = Environment.GetEnvironmentVariable("SMTP_CONTACT_RECIPIENT") ?? string.Empty;
+    }
+    
     public bool SendMessage(INotifyMessage notifyMessage)
     {
-        var host = Environment.GetEnvironmentVariable("SMTP_HOST") ?? string.Empty;
-        var port = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT") ?? string.Empty);
-        var username = Environment.GetEnvironmentVariable("SMTP_USERNAME") ?? string.Empty;
-        var password = Environment.GetEnvironmentVariable("SMTP_PASSWORD") ?? string.Empty;
-        var from = Environment.GetEnvironmentVariable("SMTP_FROM") ?? string.Empty;
-        
-        using var mailClient = new SmtpClient(host, port);
-        mailClient.Credentials = new NetworkCredential(username, password);
+        using var mailClient = new SmtpClient(_host, _port);
+        mailClient.EnableSsl = true;
+        mailClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        mailClient.Credentials = new NetworkCredential(_username, _password);
         using var message =
             new MailMessage(
                 notifyMessage.Sender, 
