f75832e53d3b54b76eaafcfd83b41082e5527794
Kim Rydén <threezool@gmail.com>
2025-08-21 11:52:11 +0200

    Added concrete classes for the service and message

---
+++ Framtid-Hbg/Service/NotifyService/EmailMessage.cs
@@ -0,0 +1,30 @@
+﻿using Framtid_hbg.Website.Models;
+using Framtid_hbg.Website.Service.Interface;
+
+namespace Framtid_hbg.Website.Service;
+
+public class EmailMessage : INotifyMessage
+{
+    public string Recipient { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public List<MemoryStream> Attachments { get; set; } = [];
+
+    public void PrepareMessage(ContactViewModel model)
+    {
+        var recipiant = Environment.GetEnvironmentVariable("SMTP_CONTACT_RECIPIENT");
+        
+        if (recipiant == null || model.Email == null || model.ContactType == null || model.Message == null)
+            throw new ArgumentException("");
+        
+        Recipient = recipiant;
+        Subject = model.ContactType;
+        Message = $"Namn: " + model.Name + Environment.NewLine;
+        Message += $"Email: " + model.Email + Environment.NewLine;
+        if (model.PhoneNumber > 999999)
+            Message += $"Telefon: " + model.PhoneNumber + Environment.NewLine;
+        if (model.Adress != null)
+            Message += $"Adress: " + model.Adress + Environment.NewLine;
+        Message += $"Meddelande: " + model.Message + Environment.NewLine;
+    }
+}
---
+++ Framtid-Hbg/Service/NotifyService/EmailService.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Framtid_hbg.Website.Service.Interface;
+
+namespace Framtid_hbg.Website.Service;
+
+public class EmailService : INotifyService
+{
+    public bool SendMessage(INotifyMessage notifyMessage)
+    {
+        var host = Environment.GetEnvironmentVariable("SMTP_HOST") ?? string.Empty;
+        var port = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT") ?? string.Empty);
+        var username = Environment.GetEnvironmentVariable("SMTP_USERNAME") ?? string.Empty;
+        var password = Environment.GetEnvironmentVariable("SMTP_PASSWORD") ?? string.Empty;
+        var from = Environment.GetEnvironmentVariable("SMTP_FROM") ?? string.Empty;
+        
+        using var mailClient = new SmtpClient(host, port);
+        mailClient.Credentials = new NetworkCredential(username, password);
+        using var message =
+            new MailMessage(
+                from, 
+                notifyMessage.Recipient, 
+                notifyMessage.Subject, 
+                notifyMessage.Message);
+
+        try
+        {
+            if (notifyMessage.Attachments.Count > 0)
+            {
+                for (var index = 0; index < notifyMessage.Attachments.Count; index++)
+                {
+                    var notifyMessageAttachment = notifyMessage.Attachments[index];
+                    message.Attachments.Add(
+                        new Attachment(
+                            notifyMessageAttachment,
+                            $"image{index + 1}.jpeg",
+                            "image/jpeg"));
+                }
+            }
+
+            mailClient.Send(message);
+        }
+        catch (SmtpException ex)
+        {
+            Console.WriteLine("Message was not sent: " + ex);
+            return false;
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Failed adding attachment: " + ex);
+            return false;
+        }
+
+        return true;
+    }
+}
