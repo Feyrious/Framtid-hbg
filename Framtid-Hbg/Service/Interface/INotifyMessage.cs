78f0632c9c4a63f39eddcb2b48ca71417bd9664e
Kim Rydén <threezool@gmail.com>
2025-08-21 11:51:22 +0200

    Added interfaces for the service

---
+++ Framtid-Hbg/Service/Interface/INotifyMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Framtid_hbg.Website.Service.Interface;
+
+public interface INotifyMessage
+{
+    public string Recipient { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public List<MemoryStream> Attachments { get; set; }
+}
