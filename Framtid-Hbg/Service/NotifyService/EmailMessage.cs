db8a859d09baaae5e23a35c3bc0aadbb55b80f71
Kim Rydén <threezool@gmail.com>
2025-08-21 13:14:52 +0200

    Changed phone number from int to string

--- Framtid-Hbg/Service/NotifyService/EmailMessage.cs
+++ Framtid-Hbg/Service/NotifyService/EmailMessage.cs
@@ -1,4 +1,4 @@
-﻿using Framtid_hbg.Website.Models;
+using Framtid_hbg.Website.Models;
 using Framtid_hbg.Website.Service.Interface;
 
 namespace Framtid_hbg.Website.Service;
@@ -21,7 +21,7 @@ public class EmailMessage : INotifyMessage
         Subject = model.ContactType;
         Message = $"Namn: " + model.Name + Environment.NewLine;
         Message += $"Email: " + model.Email + Environment.NewLine;
-        if (model.PhoneNumber > 999999)
+        if (model.PhoneNumber != null)
             Message += $"Telefon: " + model.PhoneNumber + Environment.NewLine;
         if (model.Adress != null)
             Message += $"Adress: " + model.Adress + Environment.NewLine;
